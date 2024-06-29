a63db4207af685cb5e6bf4846e7946feabcae942
Vaibhav6545 <vaibhav.vilasshelar2@cognizant.com>
2024-06-29 10:13:54 +0530

    Update OrderControllerTest.java

--- src/test/java/com/example/demo/Controller/OrderControllerTest.java
+++ src/test/java/com/example/demo/Controller/OrderControllerTest.java
@@ -1,30 +1,28 @@
-package com.example.demo.Controller;
+package minishopper.Controller;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import com.example.demo.Service.OrderService;
-import com.example.demo.dtos.CreateOrderRequest;
-import com.example.demo.dtos.OrderDto;
-import com.example.demo.dtos.UserDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.sql.Date;
-import java.time.LocalDate;
+import minishopper.Service.OrderService;
+import minishopper.Service.ProductService;
+import minishopper.dtos.CreateOrderRequest;
+import minishopper.dtos.ExcelOrder;
+import minishopper.dtos.UpdateOrderItem;
+
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.aot.DisabledInAotMode;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ContextConfiguration(classes = {OrderController.class})
@@ -37,40 +35,17 @@ class OrderControllerTest {
     @MockBean
     private OrderService orderService;
 
-    /**
-     * Method under test: {@link OrderController#createOrder(CreateOrderRequest)}
-     */
+    @MockBean
+    private ProductService productService;
+
+    
     @Test
     void testCreateOrder() throws Exception {
-        // Arrange
-        Date createdAt = mock(Date.class);
-        when(createdAt.getTime()).thenReturn(10L);
-        OrderDto.OrderDtoBuilder createdAtResult = OrderDto.builder().city("Oxford").createdAt(createdAt);
-        OrderDto.OrderDtoBuilder orderIdResult = createdAtResult.deliveredDate(LocalDate.of(1970, 1, 1))
-                .orderAmount(10.0d)
-                .orderId("42");
-        OrderDto.OrderDtoBuilder stateResult = orderIdResult.orderItems(new ArrayList<>())
-                .orderName("Order Name")
-                .orderNumber("42")
-                .orderStatus("Order Status")
-                .paymentStatus("Payment Status")
-                .postalCode("Postal Code")
-                .shippingAddress("42 Main St")
-                .shippingPhone("6625550144")
-                .state("MD");
-        UserDto user = UserDto.builder()
-                .city("Oxford")
-                .email("jane.doe@example.org")
-                .firstName("Jane")
-                .image("Image")
-                .lastName("Doe")
-                .pinCode("Pin Code")
-                .state("MD")
-                .street("Street")
-                .userId("42")
-                .build();
-        OrderDto buildResult = stateResult.user(user).build();
-        when(orderService.createOrder(Mockito.<CreateOrderRequest>any())).thenReturn(buildResult);
+        
+       
+        Object[] uriVariables = new Object[]{};
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.post("/orders", uriVariables)
+                .contentType(MediaType.APPLICATION_JSON);
 
         CreateOrderRequest createOrderRequest = new CreateOrderRequest();
         createOrderRequest.setCartId("42");
@@ -79,152 +54,181 @@ class OrderControllerTest {
         createOrderRequest.setOrderStatus("Order Status");
         createOrderRequest.setPaymentStatus("Payment Status");
         createOrderRequest.setPostalCode("Postal Code");
+        createOrderRequest.setProductId("42");
+        ArrayList<ExcelOrder> products = new ArrayList<>();
+        createOrderRequest.setProducts(products);
+        createOrderRequest.setQuantity(1);
         createOrderRequest.setShippingAddress("42 Main St");
         createOrderRequest.setShippingPhone("6625550144");
         createOrderRequest.setState("MD");
         createOrderRequest.setUserId("42");
-        String content = (new ObjectMapper()).writeValueAsString(createOrderRequest);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/orders")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content);
 
-        // Act and Assert
-        MockMvcBuilders.standaloneSetup(orderController)
-                .build()
-                .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"orderId\":\"42\",\"orderNumber\":\"42\",\"orderStatus\":\"Order Status\",\"paymentStatus\":\"Payment Status\","
-                                        + "\"orderAmount\":10.0,\"orderName\":\"Order Name\",\"shippingAddress\":\"42 Main St\",\"postalCode\":\"Postal"
-                                        + " Code\",\"city\":\"Oxford\",\"state\":\"MD\",\"shippingPhone\":\"6625550144\",\"deliveredDate\":[1970,1,1],\"createdAt"
-                                        + "\":10,\"orderItems\":[],\"user\":{\"userId\":\"42\",\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane.doe"
-                                        + "@example.org\",\"address\":null,\"street\":\"Street\",\"city\":\"Oxford\",\"state\":\"MD\",\"pinCode\":\"Pin"
-                                        + " Code\",\"image\":\"Image\"}}"));
+        ObjectMapper objectMapper = new ObjectMapper();
+        MockHttpServletRequestBuilder requestBuilder = contentTypeResult
+                .content(objectMapper.writeValueAsString(createOrderRequest));
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
+
+      
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+        
     }
 
-    /**
-     * Method under test: {@link OrderController#getOrderByOrderId(String)}
-     */
+   
+    @Test
+    void testCreateOrderForSingleProduct() throws Exception {
+       
+        Object[] uriVariables = new Object[]{};
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.post("/orders/singleProduct", uriVariables)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        createOrderRequest.setCartId("42");
+        createOrderRequest.setCity("Oxford");
+        createOrderRequest.setOrderName("Order Name");
+        createOrderRequest.setOrderStatus("Order Status");
+        createOrderRequest.setPaymentStatus("Payment Status");
+        createOrderRequest.setPostalCode("Postal Code");
+        createOrderRequest.setProductId("42");
+        ArrayList<ExcelOrder> products = new ArrayList<>();
+        createOrderRequest.setProducts(products);
+        createOrderRequest.setQuantity(1);
+        createOrderRequest.setShippingAddress("42 Main St");
+        createOrderRequest.setShippingPhone("6625550144");
+        createOrderRequest.setState("MD");
+        createOrderRequest.setUserId("42");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MockHttpServletRequestBuilder requestBuilder = contentTypeResult
+                .content(objectMapper.writeValueAsString(createOrderRequest));
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
+
+        
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+      
+    }
+
+  
+    @Test
+    void testDeleteItem() throws Exception {
+     
+        Object[] uriVariables = new Object[]{1};
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/orders/item/{orderItemId}",
+                uriVariables);
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
+
+     
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+    
+    }
+
+   
+    @Test
+    void testFetchAllProductsForExcel() throws Exception {
+    
+        Object[] uriVariables = new Object[]{};
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/orders/excel", uriVariables);
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
+
+      
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+   
+    }
+
+    
     @Test
     void testGetOrderByOrderId() throws Exception {
-        // Arrange
-        Date createdAt = mock(Date.class);
-        when(createdAt.getTime()).thenReturn(10L);
-        OrderDto.OrderDtoBuilder createdAtResult = OrderDto.builder().city("Oxford").createdAt(createdAt);
-        OrderDto.OrderDtoBuilder orderIdResult = createdAtResult.deliveredDate(LocalDate.of(1970, 1, 1))
-                .orderAmount(10.0d)
-                .orderId("42");
-        OrderDto.OrderDtoBuilder stateResult = orderIdResult.orderItems(new ArrayList<>())
-                .orderName("Order Name")
-                .orderNumber("42")
-                .orderStatus("Order Status")
-                .paymentStatus("Payment Status")
-                .postalCode("Postal Code")
-                .shippingAddress("42 Main St")
-                .shippingPhone("6625550144")
-                .state("MD");
-        UserDto user = UserDto.builder()
-                .city("Oxford")
-                .email("jane.doe@example.org")
-                .firstName("Jane")
-                .image("Image")
-                .lastName("Doe")
-                .pinCode("Pin Code")
-                .state("MD")
-                .street("Street")
-                .userId("42")
-                .build();
-        OrderDto buildResult = stateResult.user(user).build();
-        when(orderService.fetchOrderByOrderId(Mockito.<String>any())).thenReturn(buildResult);
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/orders/{orderId}", "42");
+      
+        Object[] uriVariables = new Object[]{"42"};
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/orders/{orderId}", uriVariables);
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
 
-        // Act and Assert
-        MockMvcBuilders.standaloneSetup(orderController)
-                .build()
-                .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"orderId\":\"42\",\"orderNumber\":\"42\",\"orderStatus\":\"Order Status\",\"paymentStatus\":\"Payment Status\","
-                                        + "\"orderAmount\":10.0,\"orderName\":\"Order Name\",\"shippingAddress\":\"42 Main St\",\"postalCode\":\"Postal"
-                                        + " Code\",\"city\":\"Oxford\",\"state\":\"MD\",\"shippingPhone\":\"6625550144\",\"deliveredDate\":[1970,1,1],\"createdAt"
-                                        + "\":10,\"orderItems\":[],\"user\":{\"userId\":\"42\",\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane.doe"
-                                        + "@example.org\",\"address\":null,\"street\":\"Street\",\"city\":\"Oxford\",\"state\":\"MD\",\"pinCode\":\"Pin"
-                                        + " Code\",\"image\":\"Image\"}}"));
+       
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+      
     }
 
-    /**
-     * Method under test: {@link OrderController#getOrderByUserId(String)}
-     */
+   
     @Test
     void testGetOrderByUserId() throws Exception {
-        // Arrange
-        when(orderService.fetchOrderByUser(Mockito.<String>any())).thenReturn(new ArrayList<>());
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/orders/user/{userId}", "42");
+     
+        Object[] uriVariables = new Object[]{"42"};
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/orders/user/{userId}", uriVariables);
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
 
-        // Act and Assert
-        MockMvcBuilders.standaloneSetup(orderController)
-                .build()
-                .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content().string("[]"));
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+     
     }
 
-    /**
-     * Method under test: {@link OrderController#getOrderByUserId(String)}
-     */
+   
     @Test
-    void testGetOrderByUserId2() throws Exception {
-        // Arrange
-        Date createdAt = mock(Date.class);
-        when(createdAt.getTime()).thenReturn(10L);
-        OrderDto.OrderDtoBuilder createdAtResult = OrderDto.builder().city("Oxford").createdAt(createdAt);
-        OrderDto.OrderDtoBuilder orderIdResult = createdAtResult.deliveredDate(LocalDate.of(1970, 1, 1))
-                .orderAmount(10.0d)
-                .orderId("42");
-        OrderDto.OrderDtoBuilder stateResult = orderIdResult.orderItems(new ArrayList<>())
-                .orderName("Order Name")
-                .orderNumber("42")
-                .orderStatus("Order Status")
-                .paymentStatus("Payment Status")
-                .postalCode("Postal Code")
-                .shippingAddress("42 Main St")
-                .shippingPhone("6625550144")
-                .state("MD");
-        UserDto user = UserDto.builder()
-                .city("Oxford")
-                .email("jane.doe@example.org")
-                .firstName("Jane")
-                .image("Image")
-                .lastName("Doe")
-                .pinCode("Pin Code")
-                .state("MD")
-                .street("Street")
-                .userId("42")
-                .build();
-        OrderDto buildResult = stateResult.user(user).build();
-        when(orderService.fetchOrderByOrderId(Mockito.<String>any())).thenReturn(buildResult);
-        when(orderService.fetchOrderByUser(Mockito.<String>any())).thenReturn(new ArrayList<>());
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/orders/user/{userId}", "",
-                "Uri Variables");
+    void testReadExcelData() throws Exception {
+     
+        Object[] uriVariables = new Object[]{};
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders.post("/orders/excel", uriVariables)
+                .contentType(MediaType.APPLICATION_JSON);
 
-        // Act and Assert
-        MockMvcBuilders.standaloneSetup(orderController)
-                .build()
-                .perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.content()
-                        .string(
-                                "{\"orderId\":\"42\",\"orderNumber\":\"42\",\"orderStatus\":\"Order Status\",\"paymentStatus\":\"Payment Status\","
-                                        + "\"orderAmount\":10.0,\"orderName\":\"Order Name\",\"shippingAddress\":\"42 Main St\",\"postalCode\":\"Postal"
-                                        + " Code\",\"city\":\"Oxford\",\"state\":\"MD\",\"shippingPhone\":\"6625550144\",\"deliveredDate\":[1970,1,1],\"createdAt"
-                                        + "\":10,\"orderItems\":[],\"user\":{\"userId\":\"42\",\"firstName\":\"Jane\",\"lastName\":\"Doe\",\"email\":\"jane.doe"
-                                        + "@example.org\",\"address\":null,\"street\":\"Street\",\"city\":\"Oxford\",\"state\":\"MD\",\"pinCode\":\"Pin"
-                                        + " Code\",\"image\":\"Image\"}}"));
+        CreateOrderRequest createOrderRequest = new CreateOrderRequest();
+        createOrderRequest.setCartId("42");
+        createOrderRequest.setCity("Oxford");
+        createOrderRequest.setOrderName("Order Name");
+        createOrderRequest.setOrderStatus("Order Status");
+        createOrderRequest.setPaymentStatus("Payment Status");
+        createOrderRequest.setPostalCode("Postal Code");
+        createOrderRequest.setProductId("42");
+        ArrayList<ExcelOrder> products = new ArrayList<>();
+        createOrderRequest.setProducts(products);
+        createOrderRequest.setQuantity(1);
+        createOrderRequest.setShippingAddress("42 Main St");
+        createOrderRequest.setShippingPhone("6625550144");
+        createOrderRequest.setState("MD");
+        createOrderRequest.setUserId("42");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MockHttpServletRequestBuilder requestBuilder = contentTypeResult
+                .content(objectMapper.writeValueAsString(createOrderRequest));
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
+
+    
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
+      
+    }
+
+  
+    @Test
+    void testUpdateItemInOrdere() throws Exception {
+        
+        Object[] uriVariables = new Object[]{};
+        MockHttpServletRequestBuilder contentTypeResult = MockMvcRequestBuilders
+                .post("/orders/updateOrderItem", uriVariables)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        UpdateOrderItem updateOrderItem = new UpdateOrderItem();
+        updateOrderItem.setOrderItemId(1);
+        updateOrderItem.setProductId("42");
+        updateOrderItem.setQuantity(1);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        MockHttpServletRequestBuilder requestBuilder = contentTypeResult
+                .content(objectMapper.writeValueAsString(updateOrderItem));
+        Object[] controllers = new Object[]{orderController};
+        MockMvc buildResult = MockMvcBuilders.standaloneSetup(controllers).build();
+
+        
+        ResultActions actualPerformResult = buildResult.perform(requestBuilder);
+
     }
 }
